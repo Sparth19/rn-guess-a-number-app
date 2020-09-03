9768f64e5f674117967db592a06c0dfe97481997
Parth <parthshekhaliya18@gmail.com>
2020-09-03 18:45:47 +0530

    Platform API change in header

--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,43 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform } from "react-native";
 import Colors from "../constants/colors";
 import TitleText from "./TitleText";
 
 const Header = (props) => {
+
+
   return (
-    <View style={styles.header}>
+    <View style={{...styles.headerBase,...Platform.select(
+      {ios:styles.headerIOS,
+      android:styles.headerAndroid})}}>
       <TitleText style={styles.headerTitle}>{props.title}</TitleText>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  header: {
+  headerBase: {
     width: "100%",
     height: 90,
     paddingTop: 50,
     paddingBottom: 20,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: Colors.primary,
+    // backgroundColor: Platform.OS === "android" ? Colors.primary : "white",
+    // borderBottomColor: Platform.OS === "android" ? "white" : "#ccc",
+    // borderBottomWidth: Platform.OS === "android" ? 0 : 1,
+  },
+  headerAndroid:{
+    backgroundColor: Colors.primary
+
+  },
+  headerIOS:{
+    backgroundColor:  "white",
+    borderBottomColor:   "#ccc",
+    borderBottomWidth: 1,
   },
   headerTitle: {
-    color: "black",
+    color: Platform.OS === "android" ? "white" : Colors.primary,
   },
 });
 
